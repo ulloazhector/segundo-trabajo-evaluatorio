18134e2a4c6425757501f05bc1be2c249a1471e4
AgustinCucurull <69664231+AgustinCucurull@users.noreply.github.com>
2022-06-12 16:13:51 +0000

    Create Cliente_v1.cpp

---
+++ 04-password-generator/Cliente_v1.cpp
@@ -0,0 +1,61 @@
+//Nota: este es el archivo sin las clases, es el basico pero que funciona bien, en este momento estoy haciendo la v2 con objetos
+
+#include <string.h> 
+#include <sys/socket.h> 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
+
+#define SERVER_ADDRESS  "192.168.0.149"     /* server IP */
+#define PORT            8080 
+
+/* Test sequences */
+char buf_tx[] = "Hello server. I am a client";      
+char buf_rx[100];                     /* receive buffer */
+ 
+ 
+/* This clients connects, sends a text and disconnects */
+int main() 
+{ 
+    int sockfd; 
+    struct sockaddr_in servaddr; 
+    
+    /* Socket creation */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) 
+    { 
+        printf("CLIENT: socket creation failed...\n"); 
+        return -1;  
+    } 
+    else
+    {
+        printf("CLIENT: Socket successfully created..\n"); 
+    }
+    
+    
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    /* assign IP, PORT */
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr( SERVER_ADDRESS ); 
+    servaddr.sin_port = htons(PORT); 
+  
+    /* try to connect the client socket to server socket */
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) 
+    { 
+        printf("connection with the server failed...\n");  
+        return -1;
+    } 
+    
+    printf("connected to the server..\n"); 
+  
+    /* send test sequences*/
+    write(sockfd, buf_tx, sizeof(buf_tx));     
+    read(sockfd, buf_rx, sizeof(buf_rx));
+    printf("CLIENT:Received: %s \n", buf_rx);
+   
+       
+    /* close the socket */
+    close(sockfd); 
+} 
